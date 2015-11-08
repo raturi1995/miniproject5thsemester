9762d91341912c630b8f070be8af9431abd9d560
raturi1995 <shub23raturi07@gmail.com>
2015-11-08 23:12:50 +0530

    input module testing

--- program.c
+++ program.c
@@ -2,7 +2,27 @@
 int main()
 {
     FILE *fp;
+    char mnemonics[15][100],temp_mnemonics[15];
+    int opcode[100],temp_opcode,i,no_of_mnemonics;
     fp=fopen("instruction_set.txt","r");
-    while(st=fgets(fp)!=EOF)
+    /*i=0;
+    do
     {
+        fscanf(fp,"%s%x",temp_mnemonics,temp_opcode);
+        mnemonics[0][i]=temp_mnemonics;
+        opcode[i]=temp_opcode;
+        i++;
+    }while(temp_mnemonics!=EOF);
+    no_of_mnemonics=i;*/
+    //fscanf(fp,"%s",temp_mnemonics);
+  while(temp_mnemonics!=EOF)
+  {
+   printf("%s\t",temp_mnemonics);
+   fscanf(fp,"%s",temp_mnemonics);
+  }
+   /* for(i=0;i<no_of_mnemonics;i++)
+    {
+        printf("%s\t%x\n",mnemonics[i],opcode[i]);
+    }*/
+    return(0);
 }
